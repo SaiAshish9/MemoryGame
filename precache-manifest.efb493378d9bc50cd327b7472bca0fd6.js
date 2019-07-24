9346ab6b1e86611dfffa0e5725e064242d599cb6
SaiAshish9 <saiashish3760@gmail.com>
2019-07-24 18:22:17 +0530

    Updates

---
+++ precache-manifest.efb493378d9bc50cd327b7472bca0fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083e409922a41d562a438d39272aad58",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "80f3d605996e621de58c",
+    "url": "/MemoryGame/static/css/main.3f45e2aa.chunk.css"
+  },
+  {
+    "revision": "d0581df5327a23d736c5",
+    "url": "/MemoryGame/static/js/2.3c931148.chunk.js"
+  },
+  {
+    "revision": "80f3d605996e621de58c",
+    "url": "/MemoryGame/static/js/main.2b383c0f.chunk.js"
+  },
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  }
+]);
